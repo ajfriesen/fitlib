b60a11be2de2bcd6583c91065f10bbd8d1468539
ajfriesen <ajfriesen@users.noreply.github.com>
2021-02-16 08:33:55 +0100

    Change Home Widget to statefull widget and add dummy method to init the state

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/data.dart';
+import 'package:flutter_app/models.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Exercise> exerciseList = [];
+
+  @override
+  void initState() {
+    // fetch data from firebase
+    // transform data into exercises
+    // put exersizes into the list
+    // call build method to refresh the widets data (set state)
+    // super.....
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MyList();
+    //return MyList();
+    return MyList(exerciseList);
   }
 }
