54738ade9d957d20142a534e1c962ccdec78a9d6
ajfriesen <ajfriesen@users.noreply.github.com>
2021-02-16 08:29:49 +0100

    Use more verbose constructor for understanding

--- lib/models.dart
+++ lib/models.dart
@@ -1,11 +1,17 @@
-import 'package:flutter/widgets.dart';
-
 class Exercise {
-  const Exercise({
-    @required this.name,
-    this.imageUrl,
-  }) : assert(name != null);
+  String name;
+  String imageUrl;
 
-  final String name;
-  final String imageUrl;
+  Exercise(re_name, re_imageUrl) {
+    name = re_name;
+    imageUrl = re_imageUrl;
+  }
+
+  //const Exercise({
+  // @required this.name,
+  //this.imageUrl,
+  //}) : assert(name != null);
+
+  // final String name;
+  //final String imageUrl;
 }
