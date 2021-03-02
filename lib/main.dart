41752e20d13361a482bc32b172c39e0e22bc8016
ajfriesen <andre.friesen@gmail.com>
2021-03-02 21:56:08 +0100

    Add provider to MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/services/repository.dart';
+import 'package:provider/provider.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -20,10 +22,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return Provider<Repository>(
+    create: (_) => Repository(FirebaseFirestore.instance),
+    child: MaterialApp(
       title: _title,
       home: MyStatefulWidget(),
-    );
+    ));
   }
 }
 
