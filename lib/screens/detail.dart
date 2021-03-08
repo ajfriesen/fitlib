cb2b55fa7b5a6ffec9cd09bc6df1f473fd4e455f
ajfriesen <andre.friesen@gmail.com>
2021-03-09 00:59:06 +0100

    Change ListView and Detail view

--- lib/screens/detail.dart
+++ lib/screens/detail.dart
@@ -1,44 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/models.dart';
 
 class Detail extends StatelessWidget {
+  //Object to handle view
+  final Exercise exercise;
+
+  Detail({required this.exercise});
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(8.0),
-                    child: Expanded(
-                      child: Text('Push Up'),
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(20.0),
-                    child: Image.asset('images/push-ups.jpg',
-                        fit: BoxFit.fitWidth),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                child: Text('Darauf achten'),
-                padding: EdgeInsets.all(20.0),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(exercise.name!),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Image.asset('images/push-ups.jpg', fit: BoxFit.fitWidth),
+        ));
   }
 }
