bfbc7e0bfcb847ac6a6951278c93a013a2f1b1ca
Andrej Friesen <andrej@gridscale.io>
2021-04-12 23:39:22 +0200

    Use imageUrl from Exersice to display detail screen and get rid off old code

--- lib/screens/detail.dart
+++ lib/screens/detail.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/models/models.dart';
-import 'package:flutter_app/services/database.dart';
-import 'package:provider/provider.dart';
 
 class Detail extends StatefulWidget {
   //Object to handle view
@@ -14,38 +12,15 @@ class Detail extends StatefulWidget {
 }
 
 class _DetailState extends State<Detail> {
-  String? _downloadUrl;
-
-  @override
-  void initState() {
-    super.initState();
-
-    String? imageName = widget.exercise.imageName;
-    if (imageName == null) {
-      return;
-    }
-
-    context.read<Database>().getDownloadUrl(imageName).then((value) {
-      setState(() {
-        _downloadUrl = value;
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    String image = 'images/push-ups.jpg';
-    if (_downloadUrl != null && _downloadUrl != "") {
-      image = _downloadUrl!;
-    }
-
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.exercise.name!),
         ),
         body: Padding(
           padding: EdgeInsets.all(16.0),
-          child: Image.asset(image, fit: BoxFit.fitWidth),
+          child: Image.network(widget.exercise.imageUrl!, fit: BoxFit.fitWidth),
         ));
   }
 }
