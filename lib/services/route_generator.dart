7257a29b391171f0b0ba42ae61afa01432fbcdcf
ajfriesen <andre.friesen@gmail.com>
2021-04-13 00:17:11 +0200

    Create Route Generator function

---
+++ lib/services/route_generator.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/add_exercise.dart';
+
+class RouterGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/addExercise':
+        return MaterialPageRoute(builder: (_) => AddExercise());
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Error'),
+        ),
+        body: Center(
+          child: Text('ERROR'),
+        ),
+      );
+    });
+  }
+}
