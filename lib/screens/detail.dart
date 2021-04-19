bed04bdbf5e9a9dc4a0ebc12d840ef4d0a0e79c6
ajfriesen <andre.friesen@gmail.com>
2021-04-19 23:01:49 +0200

    Add image_picker and PopupMenuButton

--- lib/screens/detail.dart
+++ lib/screens/detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/models/models.dart';
+import 'package:flutter_app/services/camera.dart';
 
 class Detail extends StatefulWidget {
   //Object to handle view
@@ -14,19 +15,29 @@ class Detail extends StatefulWidget {
 class _DetailState extends State<Detail> {
   static const String placeholderImage = 'images/placeholder.png';
 
+  final media = Media();
+
   @override
   Widget build(BuildContext context) {
     String imageurl = widget.exercise.imageUrl ?? placeholderImage;
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.exercise.name!),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: imageurl == placeholderImage || imageurl.isEmpty
-              ? Image.asset(placeholderImage, fit: BoxFit.fitWidth)
-              : Image.network(imageurl, fit: BoxFit.fitWidth),
-        ));
+      appBar: AppBar(
+        title: Text(widget.exercise.name!),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: imageurl == placeholderImage || imageurl.isEmpty
+            ? Image.asset(placeholderImage, fit: BoxFit.fitWidth)
+            : Image.network(imageurl, fit: BoxFit.fitWidth),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          media.getMedia();
+        },
+        child: const Icon(Icons.add_photo_alternate),
+        tooltip: 'Pick Image',
+      ),
+    );
   }
 }
