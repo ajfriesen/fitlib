b9231aeb3b11fae72c468bb0a7c23d8e99275993
Andrej Friesen <andrej@gridscale.io>
2021-04-27 22:44:53 +0200

    Move Future and switch statement inside Media class

--- lib/services/media_file_service.dart
+++ lib/services/media_file_service.dart
@@ -20,42 +20,54 @@ class Media {
   }
 }
 
-Future<void> chooseImagePicker(BuildContext context) async {
-  CameraChooser? index = await showDialog<CameraChooser>(
-      context: context,
-      builder: (BuildContext context) {
-        return SimpleDialog(
-          title: const Text('Select assignment'),
-          children: <Widget>[
-            SimpleDialogOption(
-              onPressed: () {
-                Navigator.pop(context, CameraChooser.image);
-              },
-              child: const Text('Take picture'),
-            ),
-            SimpleDialogOption(
-              onPressed: () {
-                Navigator.pop(context, CameraChooser.video);
-              },
-              child: const Text('Record video'),
-            ),
-          ],
-        );
-      });
+  // ? because we might return null
+  Future<PickedFile?> chooseImagePicker(BuildContext context) async {
+    CameraChooser? index = await showDialog<CameraChooser>(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: const Text('Choose your Image source'),
+            children: <Widget>[
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, CameraChooser.image);
+                },
+                child: const Text('Take picture'),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, CameraChooser.imageGallery);
+                },
+                child: const Text('Choose image from gallery'),
+              ),
+            ],
+          );
+        });
 
-  Media mediaService = Media();
+    switch (index) {
+      case CameraChooser.image:
+        return await getCameraImage();
+      // print(file.path);
+      // PreferencesService.saveFile();
+      // break;
+      case CameraChooser.video:
+        // ...
+        return await getCameraImage();
+      // print("camera");
+      // break;
+      case CameraChooser.videoGallery:
+        // ...
+        return await getCameraImage();
+      // break;
+      case CameraChooser.imageGallery:
+        return await getGalleryImage();
+      // print(file.path);
 
-  switch (index) {
-    case CameraChooser.image:
-      PickedFile file = await mediaService.getCameraImage();
-      print(file.path);
-      break;
-    case CameraChooser.video:
-      // ...
-      print("camera");
-      break;
-    case null:
-      // dialog dismissed
-      break;
+      // break;
+      case null:
+        return null;
+      //
+
+    }
   }
 }
