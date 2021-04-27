ca5b0127a26cfee02bf123a02606a94492545171
Andrej Friesen <andrej@gridscale.io>
2021-04-27 22:42:55 +0200

    Create constructor and constructor function for PreferencesService
    Since SharedPreferences.getInstance() will provide a Future and this needs async. Constructors can not be async.
    Therefore the workaround with the function.

--- lib/services/shared_preferences_service.dart
+++ lib/services/shared_preferences_service.dart
@@ -1,9 +1,18 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PreferencesService {
-  SharedPreferences settings;
+  // needs to be late because of async
+  late SharedPreferences settings;
 
-  saveFile(String path) {}
+  // constructor
+  PreferencesService() {
+    _createSettings();
+  }
+
+  // we need a function because a constructor can not be async
+  _createSettings() async {
+    settings = await SharedPreferences.getInstance();
+  }
 
   getFile() {}
 }
