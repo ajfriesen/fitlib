bc308995983861de94e1930dd3cadfc78034866f
Andrej Friesen <andrej@gridscale.io>
2021-05-15 17:07:32 +0200

    Change to singleton for sharedPreferences

--- lib/services/shared_preferences_service.dart
+++ lib/services/shared_preferences_service.dart
@@ -2,27 +2,31 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class PreferencesService {
   // needs to be late because of async
-  SharedPreferences? settings;
+  // SharedPreferences? settings;
 
   // constructor
-  PreferencesService() {
-    _createSettings();
-  }
+  // PreferencesService() {
+  //   _createSettings();
+  // }
 
   // we need a function because a constructor can not be async
-  _createSettings() async {
-    settings = await SharedPreferences.getInstance();
-    print(settings);
-  }
+  // _createSettings() async {
+  //   settings = await SharedPreferences.getInstance();
+  //   print(settings);
+  // }
 
-  Future<bool> saveImageMetadata(String storedImage, String key) {
+  Future<bool> saveImageMetadata(String storedImage, String key) async {
+    final SharedPreferences settings = await SharedPreferences.getInstance();
+
     if (settings != null) {
-      return settings!.setString(key, storedImage);
+      return settings.setString(key, storedImage);
     }
     return Future.value(false);
   }
 
-  String? getFile(String key) {
-    return settings?.getString(key);
+  Future<String?> getFile(String key) async {
+    final SharedPreferences settings = await SharedPreferences.getInstance();
+
+    return settings.getString(key);
   }
 }
