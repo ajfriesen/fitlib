416bddf2cf2f8026efa41a77862b1aebef565a5f
ajfriesen <andre.friesen@gmail.com>
2021-05-24 21:14:38 +0200

    Add empty workout list example

---
+++ lib/screens/workout_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WorkoutList extends StatefulWidget {
+  // const WorkoutList({Key key}) : super(key: key);
+
+  @override
+  _WorkoutListState createState() => _WorkoutListState();
+}
+
+class _WorkoutListState extends State<WorkoutList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
