e63ce970a561c0fc6a55614e1e3856007e751004
ajfriesen <andre.friesen@gmail.com>
2021-05-24 21:15:31 +0200

    Add example form for add exercise screen

--- lib/screens/add_exercise.dart
+++ lib/screens/add_exercise.dart
@@ -8,7 +8,53 @@ class AddExercise extends StatelessWidget {
       appBar: AppBar(
         title: Text('Add your Exersize'),
       ),
-      body: Center(child: Text('Here will be the Add Exersice Form')),
+      body: Column(
+        children: [
+          Form(
+              child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.fitness_center),
+                  hintText: 'Give a good name',
+                  labelText: 'Title *',
+                ),
+                onSaved: (String? value) {
+                  // This optional block of code can be used to run
+                  // code when the user saves the form.
+                },
+                validator: (String? value) {
+                  return (value != null && value.contains('@'))
+                      ? 'Do not use the @ char.'
+                      : null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.description_outlined),
+                  hintText: 'Some tips for the exercise',
+                  labelText: 'Description',
+                ),
+                onSaved: (String? value) {
+                  // This optional block of code can be used to run
+                  // code when the user saves the form.
+                },
+                validator: (String? value) {
+                  return (value != null && value.contains('@'))
+                      ? 'Do not use the @ char.'
+                      : null;
+                },
+              )
+              //TODO: Add image Upload
+              //TODO: Add dropdown from options
+            ],
+          ))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.save),
+      ),
     );
   }
 }
