220df5fe950676aaf621aae150fb098abcc6c17c
Andrej Friesen <andrej@gridscale.io>
2021-07-16 23:07:44 +0200

    Add fromJson and toJson to Exercise model

--- lib/models/exercise.dart
+++ lib/models/exercise.dart
@@ -13,4 +13,22 @@ class Exercise {
       this.description});
 
   Exercise.empty();
+
+  Exercise.fromJson(Map<String, Object?> json)
+      : this(
+            id: json['id']! as String,
+            name: json['name']! as String,
+            imageName: json['imageName']! as String,
+            imageUrl: json['imageUrl']! as String,
+            description: json['description']! as String);
+
+  Map<String, Object?> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'imageName': imageName,
+      'imageUrl': imageUrl,
+      'description': description,
+    };
+  }
 }
