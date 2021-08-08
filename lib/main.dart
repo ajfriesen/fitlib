e114ca2b6c9f964bdd52f03496876839db49b7c9
ajfriesen <andre.friesen@gmail.com>
2021-08-08 19:08:55 +0200

    Rename exercise_list_page.dart to home_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/screens/exercise_list_page.dart';
+import 'package:flutter_app/screens/home_screen.dart';
 import 'package:provider/provider.dart';
 
 import 'components/exercise/exercise_view_model.dart';
