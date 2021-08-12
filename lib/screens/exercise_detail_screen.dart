2c110d3fab437e5beac27f7efec41f3f3e130aa5
ajfriesen <andre.friesen@gmail.com>
2021-08-13 00:36:39 +0200

    Update Retail screen

--- lib/screens/exercise_detail_screen.dart
+++ lib/screens/exercise_detail_screen.dart
@@ -1,73 +1,34 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/components/custom_image.dart';
+import 'package:flutter_app/components/exercise/exercise_list_view_model.dart';
 import 'package:flutter_app/models/exercise.dart';
-import 'package:flutter_app/services/authentication.dart';
-import 'package:flutter_app/services/media_file_service.dart';
 import 'package:flutter_app/services/shared_preferences_service.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_app/models/exercise_image_list.dart';
 
 class Detail extends StatefulWidget {
-  //Object to handle view
   final Exercise exercise;
 
   Detail({required this.exercise});
 
   @override
-  _DetailState createState() => _DetailState();
+  State createState() {
+    //TODO: Why not return DetailState(exercise)?
+    return DetailState();
+  }
 }
+class DetailState extends State<Detail> {
 
-class _DetailState extends State<Detail> {
-  late Login loginProvider;
+  ExerciseListViewModel? exerciseViewModel;
+
   static const String placeholderImage = 'images/placeholder.png';
-  final Media mediaService = Media();
   final PreferencesService preferencesServiceService = PreferencesService();
 
   String? imagePath;
-  String? userId;
-  UserData testData = UserData();
+
   List<CustomExerciseData> localImageDataList = List.empty();
 
   @override
-  void initState() {
-    loginProvider = context.read<Login>();
-    userId = loginProvider.getUser()?.uid;
-    super.initState();
-
-    // preferencesServiceService.getFile(loginProvider.getUser()?.uid ?? '').then((value) {
-    //   setState(() {
-    //     imagePath = value;
-    //   });
-    // });
-
-    preferencesServiceService.read(userId: userId!).then((value) {
-      setState(() {
-        //TODO: elvis operator value?.imageData
-        CustomExerciseData foundImageData = CustomExerciseData(imagePath: '');
-        if (value?.customExercises != null) {
-          List<CustomExerciseData>? localImageData = value!.customExercises;
-
-          foundImageData = localImageData!.firstWhere((element) {
-            if (element.exerciseName != widget.exercise.name) {
-              return false;
-            }
-            return true;
-          }, orElse: () => foundImageData
-              // orElse: () => ImageData(imagePath: '')
-              );
-        }
-
-        imagePath = foundImageData.imagePath;
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    String imageurl = widget.exercise.imageUrl ?? placeholderImage;
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.exercise.name!),
@@ -78,9 +39,9 @@ class _DetailState extends State<Detail> {
           child: Center(
             child: Column(
               children: [
-                imageurl == placeholderImage || imageurl.isEmpty
+                widget.exercise.imageUrl == "" || widget.exercise.imageUrl == null
                     ? Image.asset(placeholderImage, fit: BoxFit.fitWidth)
-                    : Image.network(imageurl, fit: BoxFit.fitWidth),
+                    : Image.network(widget.exercise.imageUrl!, fit: BoxFit.fitWidth),
                 SizedBox(height: 40),
                 Text(
                   "Description",
@@ -88,7 +49,7 @@ class _DetailState extends State<Detail> {
                 ),
                 widget.exercise.description != null || widget.exercise.description != ""
                     ? Text(widget.exercise.description!)
-                    : Text("Empty description"),
+                    : Text("Keine Beschreibung"),
                 SizedBox(height: 40),
                 Text(
                   'My saved Images',
@@ -104,32 +65,6 @@ class _DetailState extends State<Detail> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          final User? loggedInUser = loginProvider.getUser();
-
-          if (loggedInUser == null) {
-            //TODO: Add Modal alert
-            return;
-          }
-
-          PickedFile? pickedImage = await mediaService.chooseImagePicker(context);
-
-          if (pickedImage != null) {
-            bool successfulSave = false;
-
-            if (userId != null) {
-              successfulSave = await preferencesServiceService.save(
-                  userId: userId!,
-                  imagePath: pickedImage.path,
-                  exerciseName: widget.exercise.name!);
-            }
-
-            if (successfulSave) {
-              setState(() {
-                imagePath = pickedImage.path;
-              });
-            }
-            //TODO: get a toast widget
-          }
         },
         child: const Icon(Icons.add_photo_alternate),
         tooltip: 'Pick Image',
