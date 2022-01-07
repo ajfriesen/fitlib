a7c8a1efaa96dc9e045c02918bf6b5af6be26d47
ajfriesen <andre.friesen@gmail.com>
2022-01-07 14:57:02 +0100

    Extend user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,7 +1,27 @@
 class User {
   String? id;
   String? email;
-  String? role;
+  DateTime? created;
+  DateTime? updated;
 
-  User({required this.id, this.email, this.role});
+  User.empty();
+
+  User({required this.id, this.email, this.created, this.updated});
+
+  User.fromJson(Map<String, dynamic> json)
+      : this(
+      id: json['id']! as String,
+      email: json['email']! as String,
+      created: json['created']! as DateTime,
+      updated: json['updated']! as DateTime);
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'email': email,
+      'created': created,
+      'updated': updated,
+    };
+  }
+
 }
